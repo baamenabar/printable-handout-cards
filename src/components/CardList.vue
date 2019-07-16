cd3723af16397ef6557889b54b49b1a17ead8f20
Agustin Amenabar <agustin.amenabar@eqs.com>
2019-07-16 20:40:24 +0200

    configuring the linter and figuring out components

---
+++ src/components/CardList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card"></div>
+</template>
+
+<script lang=ts>
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class CardList extends Vue {
+  @Prop name: string;
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
