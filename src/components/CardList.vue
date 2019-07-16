0034e05af191d3523f61251c20b7cd3c8f1a2521
Agustin Amenabar <agustin.amenabar@eqs.com>
2019-07-16 23:00:11 +0200

    Initial templates. Further linter configurations. Create mock data.

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="card"></div>
+  <div class="list">
+    <CardComponent v-for="card in list" :card="card" :key="card.slug"></CardComponent>
+  </div>
 </template>
 
 <script lang=ts>
 import { Component, Vue, Prop } from 'vue-property-decorator';
+import cardListData from '../assets/cards.json';
+import CardComponent, { Card } from './Card.vue';
 
-@Component
-export default class CardList extends Vue {
-  @Prop name: string;
+@Component({ components: { CardComponent } })
+export default class CardListComponent extends Vue {
+    list!: Card[];
+    constructor() {
+        super();
+        this.list = cardListData;
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.list {
+    display: flex;
+}
 </style>
