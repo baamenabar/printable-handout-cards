f0bde4509a5fdea36117fa9f7d48708078f703bf
Agustin Amenabar <agustin.amenabar@eqs.com>
2019-11-09 15:42:29 +0100

    feat: created store options mock.

--- src/components/CardList.spec.ts
+++ src/components/CardList.spec.ts
@@ -1,20 +1,19 @@
-import {
-    Wrapper,
-    shallowMount,
-    createLocalVue,
-    VueClass,
-} from '@vue/test-utils';
+import { Wrapper, shallowMount, createLocalVue } from '@vue/test-utils';
 import CardListComponent from './CardList.vue';
+import { storeOptionsMock } from '@/store/store.mock';
 import Vuex from 'vuex';
 
 describe('CardList', () => {
     let wrapper: Wrapper<CardListComponent>;
-    let localVue;
+
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
 
     beforeEach(() => {
-        wrapper = shallowMount<CardListComponent>(CardListComponent);
-        localVue = createLocalVue();
-        localVue.use(Vuex);
+        wrapper = shallowMount<CardListComponent>(CardListComponent, {
+            store: new Vuex.Store(storeOptionsMock),
+            localVue,
+        });
     });
     it('should ', () => {
         expect('hello').toBeTruthy();
